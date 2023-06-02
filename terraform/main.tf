cd2b39da104b79841a87c9c3e3f511785c7ae66b
Fridgemagnet1 <samhakkak@hotmail.co.uk>
2023-06-02 10:35:18 +0100

    added backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,13 @@ resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   name     = random_pet.rg_name.id
 }
+
+# Remote backend
+terraform {
+  backend "azurerm" {
+    resource_group_name = "terraform-storage-rg"
+    storage_account_name = "terraformstatesh123"
+    container_name = "tfstatefiles"
+    key = "terraform.tfstate"
+  }
+}
